2a2a8fcbb4260c87a30d453f5d5f4b1d10529d80
Jonathan Lau <jonlau228@hotmail.com>
2018-12-07 20:23:01 -0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.hidden = (torch.zeros(1, 1, hidden_size), torch.zeros(1, 1, hidden_size))
+    
+    def forward(self, features, captions):
+        embeds = self.word_embeddings(captions[:,:-1])
+        inputs = torch.cat((features.unsqueeze(1), embeds), 1)
+        lstm_out, self.hidden = self.lstm(inputs)
+        outputs = self.linear(lstm_out)
+        return outputs
+
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        ids = []
+        for i in range(max_len):
+            predicted, states = self.lstm(inputs, states)
+            predicted = self.linear(predicted.squeeze(1))
+            target_idx = predicted.max(1)[1]
+            ids.append(target_idx.item())
+            inputs = self.word_embeddings(target_idx).unsqueeze(1)
+        return ids
